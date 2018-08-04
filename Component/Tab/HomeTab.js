7c440fba3e1eeb5d3877c246f80a2f5f32046a28
awm <awm@awmui-Mac.local>
2018-08-04 18:44:53 +0900

    ionicons add & edit
    ionicons add
    HomeTab.js - Setting 버튼 추가
    Setting.js add

--- Component/Tab/HomeTab.js
+++ Component/Tab/HomeTab.js
@@ -6,58 +6,66 @@ import {
 } from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import {Icon} from 'native-base';
+import Setting from './Inner/Setting';
 
 class HomeTab extends Component{
-    static navigationOptions = {
-        tabBarIcon: ({ tintColor }) => (
-            < Icon name = 'ios-home' style={{color:tintColor}} />
-        )
-    }
-    render(){
-        return(
-           <RootStack/>
-        );
-    }
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      < Icon name = 'ios-home' style={{color:tintColor}} />
+    )
+  }
+  render(){
+      return(
+          <RootStack/>
+      );
+  }
 }
 class HomeScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Home Screen</Text>
-          <Button
-            title="Go to Details"
-            onPress={() => this.props.navigation.navigate('Details')}
-          />
-        </View>
-      );
-    }
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
   }
+}
   
-  class DetailsScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Details Screen</Text>
-          <Button
-            title="Go to Details... again"
-            onPress={() => this.props.navigation.push('Details')}
-          />
-          <Button
-            title="Go to Home"
-            onPress={() => this.props.navigation.navigate('Home')}
-          />
-          <Button
-            title="Go back"
-            onPress={() => this.props.navigation.goBack()}
-          />
-        </View>
-      );
-    }
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go to Setting"
+          onPress={() => this.props.navigation.navigate('Setting')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
   }
+}
 const RootStack = StackNavigator(
     {
       Home: {
         screen: HomeScreen,
+      },
+      Setting: {
+        screen: Setting,
       },
       Details: {
         screen: DetailsScreen,
@@ -69,4 +77,3 @@ const RootStack = StackNavigator(
   );
 
 export default HomeTab;
-
