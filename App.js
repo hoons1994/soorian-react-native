ff60331b64e63170b9b9f790d9ddd3a595a190f2
kbw2204 <kbw2204@naver.com>
2018-08-04 17:15:37 +0900

    add_Login_View
    add Login View
    edit index.js

--- App.js
+++ App.js
@@ -1,49 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-// test
+// npm install react-navigation
+// npm install react-base
+
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+// import {createStackNavigator} from 'react-navigation';
+// import {StyleSheet, Text, View} from 'react-native';
+import MainScreen from './Component/MainScreen';
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <MainScreen/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// const AppStackNavigator = createStackNavigator({
+//   Main: {
+//     screen: MainScreen
+//   }
+  
+// })
---
+++ Component/MainScreen.js
@@ -0,0 +1,60 @@
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Platform} from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation';
+import HomeTab from './Tab/HomeTab';
+import SearchTab from './Tab/SearchTab';
+import AddMediaTab from './Tab/AddMediaTab';
+import LikesTab from './Tab/LikesTab';
+import ProfileTab from './Tab/ProfileTab';
+class MainScreen extends Component {
+    // static navigationOptions = {
+    //     headre: null
+    //     // headerLeft:  <Icon name = 'ios-camera-outline' style={{paddingLeft:10}}/> ,
+    //     // title: 'Instagram',
+    //     // headerRight: <Icon name='ios-send-outline' style={{paddingRight:10}}/>,
+    // }
+
+  render() {
+    return (
+        <AppTabNavigator/>
+    );
+  }
+}
+export default MainScreen;
+
+const AppTabNavigator = createBottomTabNavigator({
+    HomeTab:{
+        screen:HomeTab
+    },
+    Search:{
+        screen:SearchTab
+    },
+    AddMedia:{
+        screen: AddMediaTab
+    },
+    Likes:{
+        screen:LikesTab
+    },
+    Profile:{
+        screen:ProfileTab
+    }
+}, {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+        style: {
+            ...Platform.select({
+                android: {
+                    backgroundColor:'white'
+                }
+                
+            })
+        },
+        activeTintColor: '#000',
+        inactiveTintColor: '#d1cece',
+        showLabel: false,
+        showIcon: true,
+    }
+})
