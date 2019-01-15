fce89a5a04bb5371ad1bff4c2b24f6bcff6ed0c5
강병우 <kbw2204@naver.com>
2019-01-16 02:20:30 +0900

    로그인 설정 추가

--- LoginVC.js
+++ LoginVC.js
@@ -1,30 +1,80 @@
-
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TextInput, TouchableHighlight,Alert} from 'react-native';
+import {StyleSheet, Image, Text, View, TextInput, TouchableHighlight, Dimensions} from 'react-native';
+import { Right } from 'native-base';
 
-
+const {width, height} = Dimensions.get("window");
 class LoginVC extends Component {
     static navigationOptions = {
-        // header: null
+        header: null
       };
       constructor(props) {
         super(props)
         this.state = {
-          user: '',
-          passwd: ''
+          userEmail:'',
+             userPassword:''
         }
       }
-      click = () =>{
-        const { user, passwd }  = this.state ;
-            // if(user=='123'&&passwd=='123'){
-            //   Alert.alert('test')
-            this.props.navigation.navigate('TabVC')
-            // }
+      _signin = () => {
+        this.props.navigation.navigate('RegisterVC')
+      }
+      _click = () =>{
+        const { userEmail, userPassword }  = this.state ;
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+        this.props.navigation.navigate('TabVC')
+        if(userEmail==""){
+          //alert("Please enter Email address");
+          this.setState({email:'Please enter Email address'})
+        }
+        
+        else if(reg.test(userEmail) === false)
+        {
+        //alert("Email is Not Correct");
+        this.setState({email:'Email is Not Correct'})
+        return false;
+          }
+    
+        else if(userPassword==""){
+        this.setState({email:'Please enter password'})
+        }
+        else{
+        
+        fetch('http://soorian.vps.phps.kr/o3o/login.php',{
+          // fetch('http://localhost/login.php',{
+          method:'post',
+          header:{
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+          },
+          body:JSON.stringify({
+            // we will pass our input data to server
+            email: userEmail,
+            password: userPassword
+          })
+          
+        })
+        .then((response) => response.json())
+         .then((responseJson)=>{
+           if(responseJson == "ok"){
+             // redirect to profile page
+             alert("Successfully Login");
+             this.props.navigation.navigate("Profile");
+           }else{
+             alert("Wrong Login Details");
+           }
+         })
+         .catch((error)=>{
+         console.error(error);
+         });
+        }
       }
       render() {
         return (
           <View style={styles.container}>
             <View style={styles.loginContainer}>
+              <Image
+              style={styles.logo}
+              source={require('./assets/soorian_horizontal.png')}
+              />
               <TextInput
                 style={styles.input}
                 placeholder={'Enter ID'}
@@ -45,9 +95,17 @@ class LoginVC extends Component {
                 <TouchableHighlight
                   style={styles.button}
                   underlayColor={'#328FE6'}
-                  onPress={this.click}
+                  onPress={this._click}
                 > 
                 <Text style={styles.label}>LOGIN</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                onPress={this._signin}
+                style={styles.signin}>
+                
+                <Text style={styles.signin}>
+                  Sign Up
+                </Text>
               </TouchableHighlight>
               </View>
             </View>
@@ -59,17 +117,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white'
     },
     loginContainer: {
+    width: width,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#0B519C'
+    },
+    logo: {
+      width: width*0.85,
+      height: height/3.6,
+      marginBottom: height/14,
     },
     input: {
-    width: 250,
+    width: width*0.8,
     color: '#555555',
-    marginTop: 5,
+    marginBottom: 10,
     padding: 10,
     height: 50,
     borderColor: '#32C5E6',
@@ -79,25 +143,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff'
     },
     button: {
-    width: 250,
+    width: width*0.8,
     height: 50,
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
     borderRadius: 5,
-    borderColor: '#328FE6',
+    borderColor: '#ffffff',
     padding: 10,
     marginTop: 10,
-    backgroundColor: 'green'
+    backgroundColor: '#0B519C'
     },
     label: {
     width: 230,
     flex: 1,
     alignSelf: 'center',
     textAlign: 'center',
-    fontSize: 20,
-    fontWeight: '600',
+    fontSize: 22,
+    fontWeight: '700',
     color: '#ffffff'
+    },
+    signin: {
+      width: width*0.8,
+      height: 50,
+      marginTop: 10,
+      marginBottom: 50,
+      paddingRight: 5,
+      fontSize: 16,
+      color: '#fff',
+      textAlign: 'right',
+      fontWeight: "500",
     }
     });
 
